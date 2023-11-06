faa82d3bb6ab60ade52286be615ce6a73f0cbea3
ConnorBieszk <147281703+pylua-dot-code@users.noreply.github.com>
2023-11-05 19:23:56 -0600

    Created basic mod

---
+++ iphonemod/Plugin.cs
@@ -0,0 +1,48 @@
+﻿using BepInEx;
+using System;
+using UnityEngine;
+using Utilla;
+
+namespace iphonemod
+{
+    [ModdedGamemode]
+    [BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        bool inRoom;
+
+        void Start()
+        {
+            Utilla.Events.GameInitialized += OnGameInitialized;
+        }
+
+        void OnEnable()
+        {
+            HarmonyPatches.ApplyHarmonyPatches();
+        }
+
+        void OnDisable()
+        {
+            HarmonyPatches.RemoveHarmonyPatches();
+        }
+
+        void OnGameInitialized(object sender, EventArgs e)
+        {}
+
+        void Update()
+        {}
+
+        [ModdedGamemodeJoin]
+        public void OnJoin(string gamemode)
+        {
+            inRoom = true;
+        }
+
+        [ModdedGamemodeLeave]
+        public void OnLeave(string gamemode)
+        {
+            inRoom = false;
+        }
+    }
+}
---
+++ iphonemod/PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace iphonemod
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.pyluadotcode.gorillatag.iphonemod";
+        public const string Name = "iphonemod";
+        public const string Version = "1.0.0";
+    }
+}
